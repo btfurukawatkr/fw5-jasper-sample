14478fdecb858bcece746ba939a0bb4b9430e792
btfurukawatkr <furukawatkr@pm.nttdata.co.jp>
2016-12-13 17:06:54 +0900

    modify for JasperReports

--- fw5-jasper-sample-web/src/main/java/com/sample/fw5/jasper/app/view/JasperPdfView.java
+++ fw5-jasper-sample-web/src/main/java/com/sample/fw5/jasper/app/view/JasperPdfView.java
@@ -1,30 +1,50 @@
 package com.sample.fw5.jasper.app.view;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.view.document.AbstractPdfView;
+import org.terasoluna.gfw.web.download.AbstractFileDownloadView;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.pdf.PdfWriter;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRCsvDataSource;
 
 @Component
-public class JasperPdfView extends AbstractPdfView {
+public class JasperPdfView extends AbstractFileDownloadView {
 
-    private static final Logger logger = LoggerFactory
-            .getLogger(JasperPdfView.class);
-
-    @Override
-	  protected void buildPdfDocument(Map<String, Object> model,
-	          Document document, PdfWriter writer, HttpServletRequest request,
-	          HttpServletResponse response) throws Exception {
-    	logger.info((String) model.get("test1"));
-    	document.add(new Paragraph((String) model.get("test1")));
+	@Override
+	protected InputStream getInputStream(Map<String, Object> model, HttpServletRequest request) throws IOException {
+		byte[] report = null;
+		try {
+			JasperReport jasperReport = JasperCompileManager.compileReport(new ClassPathResource("/report/sample.jrxml").getInputStream());
+			Map<String, Object> params = new HashMap<>();
+			JRCsvDataSource csvData = new JRCsvDataSource(new ClassPathResource("/report/data/data.csv").getInputStream());
+			csvData.setUseFirstRowAsHeader(true);
+			JasperPrint print = JasperFillManager.fillReport(jasperReport, params, csvData);
+			report = JasperExportManager.exportReportToPdf(print);
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+		return new ByteArrayInputStream(report);
 	}
+
+	@Override
+	protected void addResponseHeader(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) {
+		response.setHeader("Content-Disposition", "attatchment; filename=sample.pdf");
+		response.setContentType("application/pdf");
+	}
+
 }
